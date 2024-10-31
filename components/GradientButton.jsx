7677d7eeb9f1733e4f8f7fb2db86594b5e70f7a9
alhandilshad <dilshadalhan@gmail.com>
2024-10-31 19:33:16 +0500

    welcome screen done

---
+++ components/GradientButton.jsx
@@ -0,0 +1,28 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+
+export default function GradientButton({ text }) {
+  return (
+    <TouchableOpacity style={{ width: '100%' }}>
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          colors={['#3b82f6', '#9333ea']}
+          style={{
+            paddingVertical: 12,
+            alignItems: 'center',
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: 18,
+          }}>
+            {text}
+          </Text>
+        </LinearGradient>
+      </TouchableOpacity>
+  )
+}
--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -1,7 +1,8 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-import { LinearGradient } from 'expo-linear-gradient';
-import { MaskedView } from '@react-native-masked-view/masked-view';
+import { LinearGradient } from 'expo-linear-gradient'
+import MaskedView from '@react-native-masked-view/masked-view';
+import GradientButton from '@/components/GradientButton'
 
 export default function WelcomeScreen() {
   return (
@@ -9,9 +10,50 @@ export default function WelcomeScreen() {
         height: '100%',
         backgroundColor: 'white',
         padding: 20,
-        paddingTop: 40
+        paddingTop: 50
     }}>
-      <Image source={require('@/assets/images/images.png')}></Image>
+      <MaskedView
+        maskElement={
+          <Text style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            color: 'black', // This color won’t display but is required for MaskedView to work.
+          }}>
+            Beep-One
+          </Text>
+        }
+      >
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          colors={['#3b82f6', '#9333ea']}
+          style={{
+            height: 40, // Adjust height to match the text component
+            justifyContent: 'center',
+          }}
+        />
+      </MaskedView>
+      <Image style={{
+        width: '100%',
+        marginTop: 30,
+        height: 240
+      }} source={require('@/assets/images/images.png')}></Image>
+      <Text style={{
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+        marginTop: 30
+      }}>Let's start the chat</Text>
+      <Text style={{
+        marginTop: 10,
+        color: '#777',
+        textAlign: 'center',
+        marginBottom: 50
+      }}>Connect with freinds and family securely and private. Enjoy!</Text>
+
+      <GradientButton text='Get Started' />
     </View>
   )
 }
