1d254dabb8c4ef2c05b9b4ae5eac3c085dc31949
alhandilshad <dilshadalhan@gmail.com>
2024-10-31 21:45:14 +0500

    sign in and sign up screens done

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,10 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ToastAndroid, Dimensions } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useNavigation, useRouter } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+import GradientButton from '../../../components/GradientButton'
 
-export default function index() {
+const { width, height } = Dimensions.get('window');
+
+export default function signIn() {
+    const navigation = useNavigation();
+    const router = useRouter();
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+
   return (
-    <View>
-      <Text>Sign In</Text>
+    <View style={{
+        padding: width * 0.06,
+        paddingTop: height * 0.05,
+        backgroundColor: 'white',
+        height: '100%',
+    }}>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={{
+        fontSize: width * 0.08,
+        marginTop: height * 0.03,
+      }}>Let's Sign You In</Text>
+      <Text style={{
+        fontSize: width * 0.08,
+        color: 'gray',
+        marginTop: height * 0.02,
+      }}>Welcome Back</Text>
+
+      <View style={{
+        marginTop: height * 0.05,
+      }}>
+        <Text>Email</Text>
+        <TextInput placeholder='Enter email' style={styles.input} />
+      </View>
+
+      <View style={{
+        marginTop: height * 0.05,
+        marginBottom: height * 0.07,
+      }}>
+        <Text>Password</Text>
+        <TextInput placeholder='Enter password' secureTextEntry={true} style={styles.input} />
+      </View>
+
+      <GradientButton text='Sign In' />
+
+      <TouchableOpacity onPress={() => router.replace('/auth/sign-up')} style={{
+        paddingVertical: height * 0.025,
+        backgroundColor: 'white',
+        color: 'white',
+        borderRadius: 15,
+        marginTop: height * 0.03,
+        borderWidth: 1.5,
+        borderColor: 'blue',
+      }}>
+        <Text style={{
+          color: 'blue',
+          textAlign: 'center',
+          fontSize: width * 0.04,
+        }}>Create Account</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    padding: height * 0.020,
+    borderWidth: 1.3,
+    borderRadius: 15,
+    borderColor: 'blue',
+  }
+})
--- app/auth/sign-up/index.js
+++ app/auth/sign-up/index.js
@@ -1,10 +1,127 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ToastAndroid, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRouter } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import GradientButton from "../../../components/GradientButton";
+import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 
-export default function index() {
+const { width, height } = Dimensions.get("window");
+
+export default function SignUp() {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const [current, setCurrent] = useState("test");
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Sign Up</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.headerText}>Create New Account</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Full Name</Text>
+        <TextInput placeholder="Enter full name" style={styles.input} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput placeholder="Enter email" style={styles.input} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          placeholder="Enter password"
+          secureTextEntry={true}
+          style={styles.input}
+        />
+      </View>
+
+      <View style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: height * 0.03,
+        marginTop: height * 0.03
+      }}>
+        <View>
+            <Text style={{
+                fontSize: 18
+            }}>Gender : </Text>
+        </View>
+      <RadioButtonGroup
+        containerStyle={{ flexDirection: 'row', gap: 5 }}
+        selected={current}
+        onSelected={(value) => setCurrent(value)}
+        radioBackground="blue"
+      >
+        <RadioButtonItem value='Male' label='Male' />
+        <RadioButtonItem value='Female' label='Female' />
+      </RadioButtonGroup>
     </View>
-  )
+
+      <GradientButton text='Create Account' />
+
+      <TouchableOpacity onPress={() => router.replace('auth/sign-in')} style={styles.signInButton}>
+        <Text style={styles.signInButtonText}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: width * 0.05,
+    paddingTop: height * 0.05,
+    backgroundColor: 'white',
+    height: "100%",
+  },
+  headerText: {
+    fontSize: width * 0.08,
+    marginTop: height * 0.03,
+  },
+  inputContainer: {
+    marginTop: height * 0.03,
+  },
+  label: {
+    fontSize: width * 0.04,
+  },
+  input: {
+    padding: height * 0.02,
+    borderWidth: 1.2,
+    borderRadius: width * 0.03,
+    borderColor: 'blue',
+    fontSize: width * 0.04,
+  },
+  createAccountButton: {
+    padding: height * 0.025,
+    backgroundColor: 'black',
+    borderRadius: width * 0.04,
+    marginTop: height * 0.06,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: "center",
+    fontSize: width * 0.045,
+  },
+  signInButton: {
+    padding: height * 0.025,
+    backgroundColor: 'white',
+    borderRadius: width * 0.04,
+    marginTop: height * 0.02,
+    borderWidth: 1.2,
+    borderColor: 'blue',
+  },
+  signInButtonText: {
+    color: 'blue',
+    textAlign: "center",
+    fontSize: width * 0.045,
+  },
+});
