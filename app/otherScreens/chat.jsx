c2c9bfbf125e39bdecb28466d27fd486f05e38e8
alhandilshad <dilshadalhan@gmail.com>
2024-11-19 21:42:51 +0500

    working on chat screen

--- app/otherScreens/chat.jsx
+++ app/otherScreens/chat.jsx
@@ -1,11 +1,12 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, KeyboardAvoidingView, Platform, FlatList, TextInput, StyleSheet, ScrollView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../../config/firebaseConfig';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { collection, query, where, onSnapshot, addDoc, deleteDoc } from 'firebase/firestore';
+import moment from 'moment';
 
 
 export default function Chat() {
@@ -15,12 +16,15 @@ export default function Chat() {
   const [currentUserEmail, setCurrentUserEmail] = useState('');
   const [currentUserUid, setCurrentUserUid] = useState('');
   const [chatUserData, setChatUserData] = useState();
+  const [message, setMessage] = useState("");
+  const [chatList, setChatList] = useState([]);
 
   useEffect(() => {
     // Fetch Current User
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setCurrentUserEmail(user.email);
+        setCurrentUserUid(user.uid);
       } else {
         console.log('User is signed out');
       }
@@ -54,10 +58,12 @@ export default function Chat() {
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
             style={{
-              height: 60,
+              height: 70,
               flexDirection: 'row',
               alignItems: 'center',
               paddingHorizontal: 15,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
             }}
           >
             <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginRight: 15 }}>
@@ -84,9 +90,14 @@ export default function Chat() {
               }
             />
 
+            <View>
             <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 22 }}>
               {chatUserData?.name || chatUser?.name}
             </Text>
+            <Text style={{ fontSize: 14, color: chatUserData?.isOnline ? 'green' : 'green' }}>
+    {chatUserData?.isOnline ? 'Online' : 'Offline'}
+  </Text>
+            </View>
           </LinearGradient>
         ),
         headerShown: true,
@@ -96,9 +107,177 @@ export default function Chat() {
     }
   }, [navigation, chatUser, chatUserData]);
 
+  useEffect(() => {
+    // Check if both state.uid and currentUserId are available
+    if (currentUserUid && chatUser?.uid) {
+      const q = query(
+        collection(db, "chat"),
+        where(chatUser?.uid,"==", true),
+        where(currentUserUid,"==", true),
+      );
+
+      const messageUnsubscribe = onSnapshot(q, (docSnap) => {
+        const list = [];
+        docSnap.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        const sortList = list.sort((a, b) => a.timestamp - b.timestamp);
+        setChatList(sortList);
+      });
+
+      return () => {
+        messageUnsubscribe(); // Unsubscribe from the snapshot listener when the component unmounts
+      };
+    }
+  }, [currentUserUid, chatUser?.uid]);
+
+  const sendMessages = () => {
+    if (!message.trim()) return;
+
+    addDoc(collection(db, "chat"), {
+      message,
+      [chatUser?.uid]: true,
+      [currentUserUid]: true,
+      senderUid: currentUserUid,
+      receiverUid: chatUser?.uid,
+      timestamp: Date.now(),
+    });
+    setMessage("");
+  };
+
+  const renderMessage = ({ item }) => {
+    const isSentMessage = item.senderUid === currentUserUid;
+  
+    return (
+      <ScrollView
+        style={[
+          styles.messageContainer,
+          isSentMessage ? { alignSelf: 'flex-end' } : { alignSelf: 'flex-start' },
+        ]}
+      >
+        {isSentMessage ? (
+          <LinearGradient
+            colors={['#3b82f6', '#9333ea']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={[styles.messageSent]}
+          >
+            <Text style={[styles.messageText, { color: 'white' }]}>{item.message}</Text>
+            <Text style={[styles.timestamp, { color: 'white' }]}>
+              {moment(item.timestamp).format('hh:mm A')}
+            </Text>
+          </LinearGradient>
+        ) : (
+          <View style={styles.messageReceived}>
+            <Text style={[styles.messageText, { color: 'black' }]}>{item.message}</Text>
+            <Text style={[styles.timestamp, { color: '#999' }]}>
+              {moment(item.timestamp).format('hh:mm A')}
+            </Text>
+          </View>
+        )}
+      </ScrollView>
+    );
+  };
+  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Chat Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      {/* Message List */}
+      <FlatList
+        data={chatList}
+        renderItem={renderMessage}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.messageList}
+      />
+
+      {/* Input Area */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type a message..."
+          placeholderTextColor="#aaa"
+          value={message}
+          onChangeText={setMessage}
+        />
+        <LinearGradient
+        colors={['#3b82f6', '#9333ea']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.sendButtonGradient}
+      >
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessages}>
+          <Ionicons name="send" size={24} color="white" />
+        </TouchableOpacity>
+      </LinearGradient>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+  },
+  messageList: {
+    flexGrow: 1,
+    paddingHorizontal: 10,
+    paddingTop: 80,
+    paddingBottom: 20
+  },
+  messageContainer: {
+    marginVertical: 5, // Minimal vertical spacing between messages
+    maxWidth: '90%', // Limits message width for better UI
+  },
+  messageSent: {
+    padding: 10,
+    borderRadius: 20,
+  },
+  messageReceived: {
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+  },
+  timestamp: {
+    alignSelf: 'flex-end',
+    fontSize: 10,
+    marginTop: 5,
+  },
+  messageText: {
+    fontSize: 15,
+    fontWeight: 500
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20
+  },
+  input: {
+    flex: 1,
+  fontSize: 16,
+  padding: 10,
+  backgroundColor: '#f5f5f5',
+  borderRadius: 25,
+  marginRight: 10,
+  borderColor: '#ddd',
+  borderWidth: 1,
+  },
+  sendButtonGradient: {
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  sendButton: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+});
